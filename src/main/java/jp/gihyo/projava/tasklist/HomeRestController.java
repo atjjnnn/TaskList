1db900ec8ca56f511b27957d473011c74d6cd844
atjjnnn <atsuki.0908@icloud.com>
2024-09-25 22:49:41 +0900

    add endopoint for adding task

--- src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -1,18 +1,37 @@
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class HomeRestController {
+
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+    private List<TaskItem> taskItems = new ArrayList<>();
+
     @RequestMapping("/resthello")
     String hello() {
         return """
                 Hello.
                 It works!
-                現在時刻は%sです。
+                現在時刻は%s`です。
                 """.formatted(LocalDateTime.now());
+    }
+
+    @GetMapping("/restadd")
+    String addItem(@RequestParam("task") String task,
+                   @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "タスクを追加しました。";
     }
 }
